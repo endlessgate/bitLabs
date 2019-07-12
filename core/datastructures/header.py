f1bcfc9a79066b6cf6364b2aa3caed23833bc117
deka <endlessgate1@gmail.com>
2019-07-13 01:18:09 +0900

    Create header.py

---
+++ core/datastructures/header.py
@@ -0,0 +1,20 @@
+
+from core.datastructures import Serializer
+
+
+class BlockHeader(Serializer):
+
+    __slots__ = ('previous_hash',
+                 'base',
+                 'number',
+                 'transaction_root',
+                 'state_root',
+                 'receipt_root',
+                 'timestamp',
+                 'cost_limit',
+                 'cost_used',
+                 'signature')
+
+    def hash(self):
+        pass
+
