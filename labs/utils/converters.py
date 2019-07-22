edd91390d4cf8f6cc5fbabb6d09fc1e4488cabc0
dekapy <endlessgate1@gmail.com>
2019-07-22 23:12:02 +0900

    big-16

--- labs/utils/converters.py
+++ labs/utils/converters.py
@@ -15,6 +15,16 @@ def int_to_big(value: int):
         raise ValueError("int to big, value expected integer, got {}".format(type(value).__name__))
 
 
+def int_to_big16(value: int):
+    if hasattr(value, 'to_bytes'):
+        try:
+            return value.to_bytes(2, byteorder='big')
+        except OverflowError:
+            raise ValueError("int to big16, value must be smaller than 65535, got {}".format(value))
+    else:
+        raise ValueError("int to big16, value expected integer, got {}".format(type(value).__name__))
+
+
 def int_from_big(value: bytes):
     if not is_bytes(value):
         raise ValueError("int from big, expected bytes, got {}".format(type(value).__name__))
